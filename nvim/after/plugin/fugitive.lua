69a54fba49c6db9b6bb1c214633cc63c0e7f6a49
llamicron <llamicron@gmail.com>
2023-04-14 13:04:42 -0500

    Updates fugitive.lua to include my git commands

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,11 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.keymap.set("n", "<leader>ga", function() 
+    vim.cmd("Git add .")
+    print("Git add .")
+end)
+
+
+vim.keymap.set("n", "<leader>gc", function() 
+    vim.cmd("Git commit")
+end)
